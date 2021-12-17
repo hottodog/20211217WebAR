33da9746daf0cc0ac7346d7735eacc4e0788daad
hottodog <62125897+hottodog@users.noreply.github.com>
2021-12-17 11:49:46 +0800

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const unityInstance = UnityLoader.instantiate("unityContainer", "Build/1217-unity.json");
+const unityInstance = UnityLoader.instantiate("unityContainer", "Build/1217-2unity.json");
 let isCameraReady = false;
 let isDetectionManagerReady = false;
 let gl = null;
